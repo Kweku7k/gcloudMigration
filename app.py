bfa4489bf67c1f3ae37bd94a87acf45e0c3701fb
mr.adumatta@gmail.com <mr.adumatta@gmail.com>
2022-11-16 17:31:57 +0000

    prestoPoll

--- app.py
+++ app.py
@@ -804,7 +804,7 @@ def naloussd():
             response = {
                 "USERID": "prestoGh",
                 "MSISDN":msisdn,
-                "MSG":"Welcome to the poll for /n A Night Under The Stars /n powered by talanku.com",
+                "MSG":"Welcome to the poll for " + "\n" + "A Night Under The Stars \n powered by talanku.com",
                 "MSGTYPE":True
             }
             resp = make_response(response)
